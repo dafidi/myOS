d423a68bae1d95bc8597b2814c439bc678b65482
DavidAwogbemila <david.awogbemila@bison.howard.edu>
2019-03-24 04:28:46 -0400

    A simple boot sector program demonstrating the stack.

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,25 +1,24 @@
-[org 0x7c00]
-the_secret:
-	db "X"
+; A simple boot sector program demonstrating the stack.
 
 mov ah, 0x0e	;indicates scrolling teletype mode for screen-related BIOS interrupt.
 
-;First attempt
-mov al, the_secret
+mov bp, 0x8000
+mov sp, bp
+
+
+push 'A'
+push 'B'
+push 'C'
+
+pop bx
+mov al, bl
 int 0x10
 
-;second attempt
-mov al, [the_secret]
+pop bx
+mov al, bl
 int 0x10
 
-;third attempt
-mov bx, the_secret
-add bx, 0x00
-mov al, [bx]
-int 0x10
-
-;fourth attempt
-mov al, [0x7c00]
+mov al, [0x7ffe]
 int 0x10
 
 jmp $
