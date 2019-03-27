fc012d351b23029fb40a35aa6a0ed05db821f472
DavidAwogbemila <david.awogbemila@bison.howard.edu>
2019-03-27 15:00:35 -0400

    A simple boot sector program to demonstrate loading data from disk using BIOS.

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,29 +1,38 @@
-; A simple boot sector program to demoinstrate segment offsetting.
+; A simple boot sector program to demonstrate using BIOS to load data from disk.
+[org 0x7c00]
 
-mov ah, 0x0e
+mov [BOOT_DRIVE], dl
 
-mov dx, ds
+; I think this is only done as a matter of good practice.
+; for our small programs, we are probably fine with wherever bp and sp
+; are placed. (commented these two lines out and still get correct results.)
+;mov bp, 0x8000
+;mov sp, bp
+
+mov bx, [where_i_want_to_load_the_data]
+mov dh, 2
+mov dl, [BOOT_DRIVE]
+call disk_load
+
+mov bx, [where_i_want_to_load_the_data]
+mov dx, [bx]
 call print_hex
-call print_endl
 
-mov bx, 0x7c0
-mov ds, bx
-mov al, [the_secret]
-int 0x10
-call print_endl
-
-mov dx, ds
+mov dx, [bx + 512]
 call print_hex
-call print_endl
 
 jmp $
 
-the_secret:
-	db "X"
-
 %include "print_string.asm"
+%include "disk_load.asm"
+
+BOOT_DRIVE: db 0
+where_i_want_to_load_the_data: dw 0xa000
 
 times 510 - ($ - $$) db 0
 
 dw 0xaa55
+
+times 256 dw 0xbeef
+times 256 dw 0xdeed
 
---
+++ disk_load.asm
@@ -0,0 +1,55 @@
+; A simple boot sector program to use BIOS to load data from disk.
+; Parameters:
+
+; EXTERNAL
+; dl => indicate (index) desired drive.
+; bx => indicate address in memory (offsetted from es) where BIOS will load the data to.
+
+; INTERNAL
+; ah => indicate BIOS read sector function.
+; al => indicate number of sectors to be read
+; ch => select cylinder (or track; i think mean the same thing)
+; dh => select the side of the desired platter => {
+; 		i.e 0 => first side of first platter
+;		    1 => second side of first platter
+;		    2 => first side of second platter ... and so on.
+;	}
+; 	 Basically, given n, floor(n/2) indicates platter index, 
+;		             n%2 indicates "top" (0) or "bottom" face (1).
+; cl => indicates index of first sector from which to start reading.
+
+; Also interrupt is 0x13.
+
+disk_load:
+	push dx
+	
+	mov ah, 0x02
+	mov al, dh
+	mov ch, 0x00
+	mov dh, 0x00
+	mov cl, 0x02
+
+	int 0x13
+	jc call_disk_error
+	pop dx
+
+	cmp dh, al
+	jne call_disk_error
+
+finish_disk_load:
+	ret
+
+call_disk_error:
+	call display_disk_error
+	ret
+
+display_disk_error:
+	push bx
+	mov bx, DISK_ERROR_MSG
+	call print_string
+	pop bx
+	ret
+
+DISK_ERROR_MSG:
+	db "Error reading from disk", 0
+
