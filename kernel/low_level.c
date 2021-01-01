978de39d774f7804772cd06d6ee1ac5f55693101
dafidi <awogbemiladavid@gmail.com>
2021-01-01 06:28:56 -0800

    Clean up low_level routines.

--- kernel/low_level.c
+++ kernel/low_level.c
@@ -1,3 +1,10 @@
+/**
+ * port_byte_in - read a byte from an I/O port.
+ * 
+ * @port: I/O port to be read from.
+ * 
+ * @returns: one byte read from an I/O port.
+ */
 unsigned char port_byte_in(unsigned short port) {
 	// Reads a byte from the specified port.
 	// "=a" (result) means: put AL register in variable RESULT when finished
@@ -7,25 +14,69 @@ unsigned char port_byte_in(unsigned short port) {
 	return result;
 }
 
-void port_byte_out(unsigned short port, unsigned char data) {
-	// "a" (data) means: load EAX with data
-	// "d" (port) means: load EDX with port
-	__asm__("out %%al, %%dx" : : "a" (data), "d" (port));
-}
-
+/**
+ * port_word_in - read a word from an I/O port.
+ * 
+ * @port: I/O port to read from.
+ * 
+ * @returns: one word read from an I/O port.
+ */
 unsigned short port_word_in(unsigned short port) {
 	unsigned short result;
 	__asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
 	return result;
 }
 
-void port_word_out(unsigned short port, unsigned char data) {
+/**
+ * port_long_in - read a double-word from an I/O port.
+ * 
+ * @port: I/O port to read from.
+ * 
+ * NOTE: untested!
+ * 
+ * @returns: one double-word read from an I/O port.
+ */
+unsigned long port_long_in(unsigned short port) {
+	unsigned long result;
+	__asm__("in %%dx, %%eax" : "=a" (result) : "d" (port));
+	return result;
+}
+
+/**
+ * port_byte_out - write a byte to an I/O port.
+ * 
+ * @port: I/O port to be written to.
+ * @data: data to write.
+ */
+void port_byte_out(unsigned short port, unsigned char data) {
+	// "a" (data) means: load EAX with data
+	// "d" (port) means: load EDX with port
 	__asm__("out %%al, %%dx" : : "a" (data), "d" (port));
 }
 
 /**
- * insb - Read nr bytes from port specified by port into starting address
- * specified by buf.
+ * port_word_out - write a byte to an I/O port.
+ * 
+ * @port: I/O port to be written to.
+ * @data: data to write.
+ */
+void port_word_out(unsigned short port, unsigned short data) {
+	__asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
+}
+
+/**
+ * port_long_out - write a double-word to an I/O port.
+ * 
+ * @port: I/O port to be written to.
+ * @data: data to write.
+ */
+void port_long_out(unsigned short port, unsigned long data) {
+	__asm__("out %%eax, %%dx" : : "a" (data), "d" (port));
+}
+
+/**
+ * insb - Read "nr" bytes from I/O port specified by "port" into memory
+ * at address specified by "buf".
  * 
  * @port: I/O port to be read from, the insb instruction expects the port
  * to be specified in EDX so we put it in there.
@@ -41,8 +92,8 @@ void insb(unsigned short port, void *buf, int nr) {
 
 /**
  * insw - Word variant of insb. See insb comment for full description.
- * Read nr words from port specified by port into starting address
- * specified by buf.
+ * Read "nr" words from I/O port specified by "port" into memory
+ * at address specified by "buf".
  * 
  * @port: I/O port to be read from.
  * @buf: The physical address of the buffer into which data should be read.
@@ -54,8 +105,8 @@ void insw(unsigned short port, void *buf, int nr) {
 
 /**
  * insl - Double-word (unconfirmed) variant of insb. See insb comment for
- * full description. Read nr words from port specified by port into starting
- * address specified by buf.
+ * full description. Read "nr" words from I/O port specified by "port" into memory
+ * at address specified by "buf".
  * 
  * @port: I/O port to be read from.
  * @buf: The physical address of the buffer into which data should be read.
--- kernel/low_level.h
+++ kernel/low_level.h
@@ -1,15 +1,19 @@
-unsigned char port_byte_in(unsigned short);
+/**
+ * Ops for reading data in from I/O ports. The underlying assembly
+ * calls read data into/out of registers. 
+ */
+unsigned char inline  port_byte_in(unsigned short);
+unsigned char inline port_word_in(unsigned short);
+void inline port_byte_out(unsigned short, unsigned char);
+void inline port_word_out(unsigned short, unsigned char);
 
-void port_byte_out(unsigned short, unsigned char);
-
-unsigned char port_word_in(unsigned short);
-
-void port_word_out(unsigned short, unsigned char);
-
-#define outsw(port, buf, nr) \
-__asm__ ("cld\nrep outsw\n\t"	\
-::"d"(port), "S"(buf), "c"(nr))
-
+/**
+ * Ops for reading data in from I/O ports. The underlying assembly
+ * calls read data into/out of memory locations. 
+ */
 void inline insb(unsigned short port, void *buf, int nr);
 void inline insw(unsigned short port, void *buf, int nr);
 void inline insl(unsigned short port, void *buf, int nr);
+void inline outsb(unsigned short port, void *buf, int nr);
+void inline outsw(unsigned short port, void *buf, int nr);
+void inline outsl(unsigned short port, void *buf, int nr);
